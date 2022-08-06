910b279859aee27b3da42d4bf8c44809f962af06
nkere30 <ninokereselidze30@gmail.com>
2022-08-06 18:59:34 +0400

    Completed

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -1,6 +1,17 @@
 package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
-    public void printGreatest(int first, int second) {
+    public static void printGreatest(int first, int second) {
+        if(first >= second) {
+            System.out.println(first);
+        } else {
+            System.out.println(second);
+        }
+    }
+
+    public static void main(String[] args) {
+        printGreatest(15, 3);
+        printGreatest(3, 8);
+        printGreatest(7, 7);
     }
 }
